4b98c1331841310e5b5db18e16f4190332e00b38
nikoo667 <dzelurinikoloz1@gmail.com>
2024-11-24 23:46:00 +0400

    commit

---
+++ day.05/CLASSWORK/classwork.py
@@ -0,0 +1,5 @@
+name1=input("enter your name:")
+lastname1=input("enter your lastname:")
+age1=input("enter your age:")
+print("Hello your name is " + name1 + " " + lastname1 + " " + "your age is " + age1)
+
---
+++ day.05/HOMEWORK/homework.py
@@ -0,0 +1,15 @@
+favsport = input("enter your favorite sport:")
+print("your favorite sport is :" + favsport) 
+
+#2
+name1=input("enter your name:")
+lastname1=input("enter your lastname:")
+age1=input("enter your age:")
+city1=input("enter your city:")
+hobby1=input("enter your hobby:")
+print("my name is" + name1 + " "+ "my lastname is " + lastname1 + " " + "my age is " + age1 + " " + "my city is " + city1 + " " + "my hobby is " + hobby1)
+ 
+ #3
+num1 = int(input("enter number1"))
+num2 = int(input("enter number2"))
+print(num1 + num2)
