7af9ce0c42f0803eb68235c96617bbff30b2deb6
nikoo667 <dzelurinikoloz1@gmail.com>
2024-12-23 22:57:48 +0400

    first commit

---
+++ day.10/classwork/index.py
@@ -0,0 +1,11 @@
+print(50<60)#true
+print(5<6)#true
+print(40<70)#true
+print(80>40)#True
+print(10<70)#true
+print()
+print(80<60)#false
+print(9<6)#false
+print(50<10)#false
+print(90>90)#false
+print(70<70)#false
