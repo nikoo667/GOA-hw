9d99537af27f630b021d3f6ad7e8ce27a70df4b2
nikoo667 <dzelurinikoloz1@gmail.com>
2025-01-10 14:52:30 +0400

    GOA BEST!

---
+++ day.14/classwork/index.py
@@ -0,0 +1,5 @@
+for i in range(1, 200):
+    if i > 23:
+        print("true")
+    else:
+        print("false")
---
+++ day.14/homework/index.py
@@ -0,0 +1,5 @@
+for number in range(1, 23456789):
+    if number < 23456:
+        print("number is low")
+    else:
+        print("number is high")
